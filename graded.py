c824423f5130c54332427d080cb7a720feedf4d4
flashfan88 <lavarjohnson20@gmail.com>
2019-08-26 16:15:59 -0500

    itried

---
+++ graded.py
@@ -0,0 +1,60 @@
+# Problem 1
+# Create a printNumbers function to print integers from -25 to 20 to the
+# console (print in the function)
+
+# def printNumbers():
+#     for num in range(-25, 21, 1):
+#         print(num)
+
+
+# printNumbers()
+
+# Problem 2
+# Create a function called checkPassword.
+# Send two string variables to the checkPassword
+# function to check if the strings are equal.
+# Return true if they are equal and false if they are not equal.
+# Print the function's return value.
+
+def checkPassword():
+    userInput = input("Enter a Password")
+    password1 = "flash"
+    if userInput != password1:
+        print("nope")
+
+
+checkPassword()
+
+
+# Problem 3
+# Write a function that determines if a number passed to it is odd or even.
+# Pass a number of your choosing (using input a good idea) and
+# then using the result from the function, print if the number was even or not.
+
+# def evenandOdd():
+
+
+
+
+# Problem 4
+# Create a function for the challenge that you call from your main
+# Create a second function that takes NO parameters
+# Create a third function that takes a single greeting parameter
+# (ex. Howdy) and returns a string using the passed in greeting and
+# 'World' (ex. Howdy World!)
+# From your first function, call the function(s) and
+# print out the final result returned
+
+
+# Problem 5:
+# We will keep having this problem until EVERYONE gets it right without help
+# Create a function that has a loop that quits with ‘q’.
+# If the user doesn't enter 'q', ask them to input another string.
+
+# def Quitting():
+#     userInput = ""
+#     while userInput != "q":
+#         userInput = input("Enter another sentence")
+#         print(userInput)
+
+# Quitting()
